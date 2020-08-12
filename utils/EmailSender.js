580148f7556c485b0d9b1246e98421de348b16aa
Ahmed0yousry <ahmedyosre13@gmail.com>
2020-08-12 17:31:25 +0200

    I just changed the private key of the mail service

--- utils/EmailSender.js
+++ utils/EmailSender.js
@@ -1,6 +1,6 @@
 const mailgun = require("mailgun-js");
 const DOMAIN = 'mail.goanapp.me';
-const api_key = "4a553526c14c41229d3b81ddf962edeb-f7d0b107-1311d563";
+const api_key = "key-351e011fa4698f9474eedd202ddc10ae";
 const mg = mailgun({ apiKey: api_key, domain: DOMAIN });
 
 module.exports = (user_email, verification_code) => {
